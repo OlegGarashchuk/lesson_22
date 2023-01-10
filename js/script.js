bf7b1c4977d2c746a9e383c2cc488eb4e393a68f
OlegGarashchuk <112845760+OlegGarashchuk@users.noreply.github.com>
2023-01-10 23:24:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict"
+
+// домашка відео №2
+//console.log(`Я вчу JS`);
+
+//console.log('Вчимо JS'); правильно
+
+//console.log('JS')
+//console.log('Вчимо') правильно, але краще після першої фрази поставити крапку з комою.
+
+//console.log('Вчимо') console.log('JS')  помилка, потрібно розділити крапкою з комою, і краще другу фразу почати з нової строки.
+
+//console.log('Вчимо);
+//console.log('JS') помилка, у першій фразі незакриті лапки.
+
+// домашка відео №3
+
+let userColorEyes;
+
+//let user;
+//let userName;
+//userName = 'Вася';
+//user = userName;
+//console.log(user);
+
+//let user = 'Вася' let age = 36 помилка, фрази потрібно розділяти комою, let пишеться один раз, в кінці треба ставити крапку з комою.
+
+//let myage = 36; написано правильно, але краще написати назву змінної myAge
+
+//const BLOCK_SIZE = 14 + 50; правильно
+//const BLOCK_HEIGHT = 100; правильно
+
+//if (true) {
+//   var size = 15;
+//}
+//console.log(size); правильно, змінна var видно за межами блоку.
+
+
+// домашка відео №4
+
+//let userAge = 36;
+//let userInfo = "Фрилансер $(userAge)"; невірно, звернення Фрилансер $(userAge) потрібно брати в зворотні лапки
+
+//let userHeight = 145 / 0;
+//console.log(userHeight); невірно, поверне значання Infinity
+
+
+//let userName;
+//console.log(typeof userName); невірно, поверне значання undefined
+
+//let userSize = "45" / "8";
+//console.log(typeof userSize); вірно
